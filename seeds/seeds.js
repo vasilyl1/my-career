4701d4b5d238f52e8c50e2b78ed4a8802961db8a
vasilyl1 <likhovaido@gmail.com>
2023-02-28 20:11:19 -0500

    bug fix

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,5 +1,5 @@
 const sequelize = require('../config/connection');
-const seedPost = require('./goalsData');
+const seedGoal = require('./goalsData');
 const seedComment = require('./commentData');
 const seedUser = require('./userData');
 
