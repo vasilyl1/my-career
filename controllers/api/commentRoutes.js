523843e4b967e50b67ded1c69d58c12d9a4e0207
Michael <fraynemichael300@gmail.com>
2023-03-02 15:48:29 -0700

    fixed formatting in commentRoutes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,57 +4,56 @@ const { ensureAuthentication } = require('../../config/passport');
 
 // CREATE(POST) a new comment
 router.post('/comment', ensureAuthentication, async (req, res) => {
-    try {
-      req.body.goal_id = req.params.id;
-      req.body.user_id = req.session.user_id;
-      const commentData = await Comment.create(req.body);
-      const comment = commentData.get({ plain: true });
-      res.status(200).json(comment);
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+  try {
+    req.body.goalId = req.params.id;
+    req.body.userId = req.session.user_id;
+    const commentData = await Comment.create(req.body);
+    const comment = commentData.get({ plain: true });
+    res.status(200).json(comment);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 // UPDATE(PUT) a comment by ID
 router.put('/comment/:id', ensureAuthentication, async (req, res) => {
-    try {
-      const commentData = await Comment.update(req.body, {
-        where: {
-          id: req.params.id,
-        },
-      });
-      if (!commentData) {
-        res.status(404).json({ message: 'No comment found with this ID' });
-        return;
+  try {
+    const commentData = await Comment.update(req.body, {
+      where: {
+        id: req.params.id
       }
-      const comment = commentData.get({ plain: true });
-      res.status(200).json(comment);
-    } catch (err) {
-      res.status(500).json(err);
+    });
+    if (!commentData) {
+      res.status(404).json({ message: 'No comment found with this ID' });
+      return;
     }
-  });
+    const comment = commentData.get({ plain: true });
+    res.status(200).json(comment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-  // DELETE a comment by ID
+// DELETE a comment by ID
 router.delete('/comment/:id', ensureAuthentication, async (req, res) => {
-    try {
-      const commentData = await Comment.destroy({
-        where: {
-          id: req.params.id,
-        },
-      });
-      if (!commentData) {
-        res.status(404).json({ message: 'No comment found with this ID' });
-        return;
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id
       }
-      
-      res.status(200).json({ message: 'comment delete success'});
-    
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
+    });
+    if (!commentData) {
+      res.status(404).json({ message: 'No comment found with this ID' });
+      return;
     }
-  });
-  
-  module.exports = router;
-  
+
+    res.status(200).json({ message: 'comment delete success'});
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
