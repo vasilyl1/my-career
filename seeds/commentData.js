d89316abfd9f3337164d996c30c23529e928bb01
Michael Frayne <fraynemichael300@gmail.com>
2023-03-02 11:09:08 -0700

    fixed formatting issues

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -4,21 +4,21 @@ const commentdata = [
   {
     body: 'This is the review for the personal objective 1',
     date: 'February 28, 2023 17:01:00',
-    goal_id: 1,
-    user_id: 2,
+    goalId: 1,
+    userId: 2
   },
   {
     body: 'I have commented my professional objective myself - reviewed performance',
     date: 'June 26, 2021 11:00:00',
-    goal_id: 1,
-    user_id: 1,
+    goalId: 1,
+    userId: 1
   },
   {
     body: 'This is something may be very stupid as this is the comment from the AI bot',
     date: 'June 23, 2021 17:00:00',
-    goal_id: 1,
-    user_id: 3,
-  },
+    goalId: 1,
+    userId: 3
+  }
 
 ];
 
