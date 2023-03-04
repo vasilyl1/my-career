7e8ade9b5c177d60477ed5b49ff98318310401b0
Michael <fraynemichael300@gmail.com>
2023-03-03 23:42:56 -0700

    working on individual card view

--- public/js/index.js
+++ public/js/index.js
@@ -14,7 +14,15 @@ $(function() {
   $('#createGoalContainer').hide();
   $( '#newGoalDate' ).datepicker();
 
-
   // const goalIds = await fetch('/api/goals').then(res => res.json()).then(data => data.map(goal => goal._id));
   // console.log(goalIds);
 });
+
+async function showPartial (element) {
+  const id = `goal${ element.id }`;
+  const source = document.getElementById(id);
+  // const template = Handlebars.compile(source);
+  // const context = element;
+  // onst html = template(context);
+  document.getElementById('partialContainer').innerHTML = await source;
+}
