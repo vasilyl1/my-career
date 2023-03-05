875ad4b81f8d476cb95157e7dcbb6f091b2af8ff
vasilyl1 <likhovaido@gmail.com>
2023-03-05 02:03:49 -0500

    add advisor initial func update

--- public/js/requestAdvisorReview.js
+++ public/js/requestAdvisorReview.js
@@ -1,10 +1,15 @@
 const requestAdvisorReview = async (event) => {
-event.preventDefault();
-const botResponse = require('../utils/chatBots');
+  event.preventDefault();
+  const botResponse = require('../utils/chatBots');
 
-// chatGPT model input
-const testResponse = await botResponse(`Provide 3 most important items on how can I achieve my development goal named as: ${goal.name}`);
+  //get the goal ID from the URL
+  //get the advisor ID from the drop down list
+  //update the goal advice field with advisor ID
+  //check if the ChatGPT ID is in the advisor side then run the bot request and update the comment
+  // chatGPT model input
+  const testResponse = await botResponse(`Provide 3 most important items on how can I achieve my development goal named as: ${ goal.name }`);
 
-//document
-//  .getElementById('addGoalButton')
-//  .addEventListener('click', requestAdvisorReview); // add event listener for when the add goal button is clicked
+};
+document
+  .getElementById('requestReview')
+  .addEventListener('click', requestAdvisorReview); // add event listener for when the add goal button is clicked
