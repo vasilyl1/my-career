8de93da463cde296a338227e31da78333824bb77
Mohamed Osman <mohamedosman@Mohameds-MacBook-Pro.local>
2023-03-05 23:49:11 -0500

    editGoalAPI.js created for api request on editbutton and goalRoutes.js minor update

--- controllers/api/goalRoutes.js
+++ controllers/api/goalRoutes.js
@@ -16,8 +16,8 @@ router.post('', withAuth, async (req, res) => {
 });
 
 
-// Update(PUT) a goal by ID router.put("/:id") -- /api/goals/:id
-router.put('/goal/:id', withAuth, async (req, res) => { // /api/goals/goal/:id
+// Update(PUT) a goal by ID
+router.put('/:id', withAuth, async (req, res) => {
   try {
     const goalData = await Goal.update(req.body, {
       where: {
